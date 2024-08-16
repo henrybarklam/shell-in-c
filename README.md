de122d27d30a4e4b76e01dfd987c9df2ef5d0902
henrybarklam <72357080+henrybarklam@users.noreply.github.com>
2024-08-15 21:36:20 -0400

    initial loop

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # shell-in-c
-Repo for following along with the Build a Shell in C tutorial found here: https://blog.ehoneahobed.com/building-a-simple-shell-in-c-part-1
+Repo for following along with the Build a Shell in C tutorial found here: https://blog.ehoneahobed.com/building-a-simple-shell-in-c-part-1 and here: https://brennan.io/2015/01/16/write-a-shell-in-c/. Both are used as either one appears to lack key implementation details found in the other
--- main.c
+++ main.c
@@ -1,5 +1,41 @@
 #include "main.h"
 
-int main(int ac, char **argv){
-    return (0);
+// Is this function written correctly? Should it be *argv[] for the list?
+// argc = argument count = lengh of the argument vector
+// argv = argument vector = array of character pointers
+// argv is a tokenized verwsion of the cli commands you put in 
+
+int main(int argc, char **argv){
+    // Load config files
+
+    // Run command loop
+    lsh_loop();
+
+    // Perform any shutdown/cleanup
+
+    return EXIT_SUCCESS;
+}
+
+/*
+Basic command handling looks like RPE:
+1. Read: Read the command from standard input
+2. Parse: Separate the command string into the program and arguments
+3. Execute: Run the parsed command
+*/
+
+void lsh_loop(void){
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+
+    } while (status);
 }
